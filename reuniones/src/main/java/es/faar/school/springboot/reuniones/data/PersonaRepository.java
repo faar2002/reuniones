57bbd82ccc7e3eb5abd03b0a85c4b52120d3bb49
faar2002 <faar2002@gmail.com>
2024-10-24 18:59:32 -0300

    se realiza ajuste para implementar alguno servicios rest con springboot

--- reuniones/src/main/java/es/faar/school/springboot/reuniones/data/PersonaRepository.java
+++ reuniones/src/main/java/es/faar/school/springboot/reuniones/data/PersonaRepository.java
@@ -1,9 +1,11 @@
 package es.faar.school.springboot.reuniones.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import es.faar.school.springboot.reuniones.models.Persona;
 
+@RepositoryRestResource(path = "personas")
 public interface PersonaRepository extends JpaRepository<Persona, Long>{
 
 }
